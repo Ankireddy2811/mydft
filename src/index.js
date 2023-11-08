e12ffff78ea15e7fd5bee86fc03e574a4a99c36a
Prince Kumar <princesd991@gmail.com>
2023-11-08 15:15:53 +0530

    Converted index.js in functional

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { ToastContainer } from 'react-toastify'; // Import ToastContainer
-import 'react-toastify/dist/ReactToastify.css'; // Import the CSS file for styling
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render (
+const Main=()=> {
+  return (
     <Provider store={store}>
-        <BrowserRouter>
-        <ToastContainer autoClose={1200} /> {/* Add the ToastContainer here */}
-             <App />
-        </BrowserRouter>
+      <BrowserRouter>
+        <ToastContainer autoClose={1200} />
+        <App />
+      </BrowserRouter>
     </Provider>
-);
+  );
+}
 
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
 serviceWorker.unregister();
